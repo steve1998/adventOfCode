8169ca51ac5727d3381e8267210f5878e90cf736
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2019-12-06 19:50:43 -0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -5,24 +5,51 @@ def calculateMassforModule(mass):
     result = math.floor((mass / 3)) - 2
     return result
 
-# main
-filepath = 'day1input.txt'
-input = []
-result = 0
+def calculateMassforModuleWhile(mass):
+    result = 0
+    divResult = math.floor((mass / 3)) - 2
 
-with open(filepath, 'r') as fp: 
-    line = fp.readline()
-    line = line.rstrip('\n')
-    input.append(int(line))
+    while divResult > 0:
+        divResult = math.floor((divResult / 3)) - 2
+        result += divResult
 
-    while line:    
+    return result
+
+def readFile():
+    input = []
+    filepath = 'day1input.txt'
+
+    with open(filepath, 'r') as fp: 
         line = fp.readline()
-        if line != '':
-            line = line.rstrip('\n')
-            input.append(int(line))
+        line = line.rstrip('\n')
+        input.append(int(line))
 
-for i in input:
-    result += calculateMassforModule(i)
+        while line:    
+            line = fp.readline()
+            if line != '':
+                line = line.rstrip('\n')
+                input.append(int(line))
 
-print(result)
+    return input
 
+# Main
+def main():
+    # Data
+    input = []
+    result = 0
+    
+    input = readFile()
+
+    # Part 1
+    for i in input:
+        result += calculateMassforModule(i)
+
+    print(result)
+
+    # Part 2
+    for i in input:
+        result += calculateMassforModuleWhile(i)
+
+    print(result)
+
+main()
